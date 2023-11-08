8059aa2200da99718fdb77460eb6445bc815d959
gkumar-pandey <gautamshekhar078@gmail.com>
2023-11-08 20:26:34 +0530

    poll and todo reducers test

--- src/ex03/pollReducer/poll.reducer.test.js
+++ src/ex03/pollReducer/poll.reducer.test.js
@@ -1,1 +1,65 @@
 import pollReducer from "./poll.reducer";
+
+describe("poll Reducer", () => {
+  it("should add new poll object", () => {
+    const intialState = {
+      polls: [{ id: 1, question: "Are you ready for study?", options: [] }],
+    };
+    const action = {
+      type: "CREATE_POLL",
+      payload: { id: 2, question: "Are you ready for party?" },
+    };
+    const updatedState = pollReducer(intialState, action);
+
+    expect(updatedState).toEqual({
+      polls: [
+        { id: 1, question: "Are you ready for study?", options: [] },
+        { id: 2, question: "Are you ready for party?", options: [] },
+      ],
+    });
+  });
+  it("should add option to the options array", () => {
+    const intialState = {
+      polls: [{ id: 1, question: "Are you ready for study?", options: [] }],
+    };
+    const action = {
+      type: "ADD_OPTION",
+      payload: { pollId: 1, optionText: "i am ready for party" },
+    };
+    const updatedState = pollReducer(intialState, action);
+    expect(updatedState).toEqual({
+      polls: [
+        {
+          id: 1,
+          question: "Are you ready for study?",
+          options: [{ text: "i am ready for party", votes: 0 }],
+        },
+      ],
+    });
+  });
+  it("should increment vote count by 1", () => {
+    const intialState = {
+      polls: [
+        {
+          id: 1,
+          question: "Are you ready for study?",
+          options: [{ text: "i am ready for party", votes: 0 }],
+        },
+      ],
+    };
+    const action = {
+      type: "VOTE",
+      payload: { pollId: 1, optionText: "i am ready for party" },
+    };
+    const updatedState = pollReducer(intialState, action);
+    expect(updatedState).toEqual({
+      polls: [
+        {
+          id: 1,
+          question: "Are you ready for study?",
+          options: [{ text: "i am ready for party", votes: 1 }],
+        },
+      ],
+    });
+  });
+});
--- src/ex03/todoReducer/todo.reducer.test.js
+++ src/ex03/todoReducer/todo.reducer.test.js
@@ -1,1 +1,62 @@
 import todoReducer from "./todo.reducer";
+
+describe("Todo Reducer Test", () => {
+  it("should add todo", () => {
+    const initialState = {
+      todos: [{ id: 1, text: "Learn testing", completed: false }],
+    };
+    const action = {
+      type: "ADD_TODO",
+      payload: {
+        id: 2,
+        text: "Learn React Redux",
+        completed: false,
+      },
+    };
+
+    const updatedState = todoReducer(initialState, action);
+
+    expect(updatedState).toEqual({
+      todos: [
+        { id: 1, text: "Learn testing", completed: false },
+        {
+          id: 2,
+          text: "Learn React Redux",
+          completed: false,
+        },
+      ],
+    });
+  });
+  it("should toggle todo ", () => {
+    const initialState = {
+      todos: [
+        { id: 1, text: "Learn testing", completed: false },
+        {
+          id: 2,
+          text: "Learn React Redux",
+          completed: false,
+        },
+      ],
+    };
+    const action = {
+      type: "TOGGLE_TODO",
+      payload: {
+        id: 2,
+        text: "Learn React Redux",
+        completed: false,
+      },
+    };
+    const updatedState = todoReducer(initialState, action);
+
+    expect(updatedState).toEqual({
+      todos: [
+        { id: 1, text: "Learn testing", completed: false },
+        {
+          id: 2,
+          text: "Learn React Redux",
+          completed: true,
+        },
+      ],
+    });
+  });
+});
