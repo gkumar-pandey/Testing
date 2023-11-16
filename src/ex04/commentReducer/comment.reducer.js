439c5c288f039b8e2586047d5026e07b86438355
gkumar-pandey <gautamshekhar078@gmail.com>
2023-11-16 16:10:27 +0530

    comment reducer test done

--- src/ex04/commentReducer/comment.reducer.js
+++ src/ex04/commentReducer/comment.reducer.js
@@ -1,5 +1,69 @@
-const initialState = {};
+const initialState = {
+  comments: [],
+};
 
-function commentReducer(state = initialState, action) {}
+function commentReducer(state = initialState, action) {
+  switch (action.type) {
+    case "ADD_COMMENT":
+      return {
+        ...state,
+        comments: [...state.comments, action.payload],
+      };
+    case "REMOVE_COMMENT":
+      return {
+        ...state,
+        comments: state.comments.filter(
+          (comment) => comment.id !== action.payload.id
+        ),
+      };
+    case "UPVOTE_COMMENT":
+      return {
+        ...state,
+        comments: state.comments.map((comment) =>
+          comment.id === action.payload.commentId
+            ? { ...comment, votesCount: comment.votesCount + 1 }
+            : comment
+        ),
+      };
+    case "DOWNVOTE_COMMENT":
+      return {
+        ...state,
+        comments: state.comments.map((comment) =>
+          comment.id === action.payload.commentId
+            ? { ...comment, votesCount: comment.votesCount - 1 }
+            : comment
+        ),
+      };
+    case "ADD_REPLY":
+      return {
+        ...state,
+        comments: state.comments.map((comment) => {
+          return comment.id === action.payload.commentId
+            ? {
+                ...comment,
+                replies: [...comment.replies, action.payload.reply],
+              }
+            : comment;
+        }),
+      };
+    case "REMOVE_REPLY":
+      return {
+        ...state,
+        comments: state.comments.map((comment) => {
+          return comment.id === action.payload.commentId
+            ? {
+                ...comment,
+                replies: comment.replies.filter(
+                  (reply) => reply.id !== action.payload.replyId
+                ),
+              }
+            : comment;
+        }),
+      };
+    default:
+      return { ...state };
+      break;
+  }
+}
 
 export default commentReducer;
--- src/ex04/commentReducer/comment.reducer.test.js
+++ src/ex04/commentReducer/comment.reducer.test.js
@@ -1,1 +1,124 @@
 import commentReducer from "./comment.reducer";
+
+describe("comment reducer", () => {
+  it("should add new comment", () => {
+    const initialState = {
+      comments: [],
+    };
+    const action = {
+      type: "ADD_COMMENT",
+      payload: { id: 1, text: "Hello", votesCount: 0, replies: [] },
+    };
+    const updatedState = commentReducer(initialState, action);
+    expect(updatedState).toEqual({
+      comments: [{ id: 1, text: "Hello", votesCount: 0, replies: [] }],
+    });
+  });
+  it("should remove comment", () => {
+    const initialState = {
+      comments: [
+        { id: 1, text: "Hello", votesCount: 0, replies: [] },
+        { id: 2, text: "World", votesCount: 0, replies: [] },
+      ],
+    };
+    const action = {
+      type: "REMOVE_COMMENT",
+      payload: { id: 1, text: "Hello", votesCount: 0, replies: [] },
+    };
+    const updatedState = commentReducer(initialState, action);
+    expect(updatedState).toEqual({
+      comments: [{ id: 2, text: "World", votesCount: 0, replies: [] }],
+    });
+  });
+  it("should increase upvote count by 1 ", () => {
+    const initialState = {
+      comments: [
+        { id: 1, text: "Hello", votesCount: 0, replies: [] },
+        { id: 2, text: "World", votesCount: 0, replies: [] },
+      ],
+    };
+    const action = {
+      type: "UPVOTE_COMMENT",
+      payload: { commentId: 2 },
+    };
+    const updatedState = commentReducer(initialState, action);
+    expect(updatedState).toEqual({
+      comments: [
+        { id: 1, text: "Hello", votesCount: 0, replies: [] },
+        { id: 2, text: "World", votesCount: 1, replies: [] },
+      ],
+    });
+  });
+  it("should decrease upvote count by 1", () => {
+    const initialState = {
+      comments: [
+        { id: 1, text: "Hello", votesCount: 0, replies: [] },
+        { id: 2, text: "World", votesCount: 2, replies: [] },
+      ],
+    };
+    const action = {
+      type: "DOWNVOTE_COMMENT",
+      payload: { commentId: 2 },
+    };
+    const updatedState = commentReducer(initialState, action);
+    expect(updatedState).toEqual({
+      comments: [
+        { id: 1, text: "Hello", votesCount: 0, replies: [] },
+        { id: 2, text: "World", votesCount: 1, replies: [] },
+      ],
+    });
+  });
+  it("should add reply to the comment", () => {
+    const initialState = {
+      comments: [
+        { id: 1, text: "Hello", votesCount: 0, replies: [] },
+        { id: 2, text: "World", votesCount: 2, replies: [] },
+      ],
+    };
+    const action = {
+      type: "ADD_REPLY",
+      payload: { commentId: 1, reply: { id: 1, text: "Hii" } },
+    };
+    const updatedState = commentReducer(initialState, action);
+    expect(updatedState).toEqual({
+      comments: [
+        {
+          id: 1,
+          text: "Hello",
+          votesCount: 0,
+          replies: [{ id: 1, text: "Hii" }],
+        },
+        { id: 2, text: "World", votesCount: 2, replies: [] },
+      ],
+    });
+  });
+  it("should remove reply from the comment ", () => {
+    const initialState = {
+      comments: [
+        {
+          id: 1,
+          text: "Hello",
+          votesCount: 0,
+          replies: [{ id: 1, reply: "Hii" }],
+        },
+        { id: 2, text: "World", votesCount: 1, replies: [] },
+      ],
+    };
+    const action = {
+      type: "REMOVE_REPLY",
+      payload: { commentId: 1, replyId: 1 },
+    };
+    const updatedState = commentReducer(initialState, action);
+    expect(updatedState).toEqual({
+      comments: [
+        {
+          id: 1,
+          text: "Hello",
+          votesCount: 0,
+          replies: [],
+        },
+        { id: 2, text: "World", votesCount: 1, replies: [] },
+      ],
+    });
+  });
+});
