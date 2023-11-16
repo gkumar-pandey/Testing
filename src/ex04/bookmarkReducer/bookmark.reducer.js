ef30b7347f849706f49e9309dba6dfeff7b06f85
gkumar-pandey <gautamshekhar078@gmail.com>
2023-11-16 13:15:07 +0530

    bookmark reducer test written

--- src/ex04/bookmarkReducer/bookmark.reducer.js
+++ src/ex04/bookmarkReducer/bookmark.reducer.js
@@ -1,5 +1,63 @@
-const initialState = {};
+const initialState = {
+  bookmarks: [],
+  filters: {},
+};
 
-function bookmarkReducer(state = initialState, action) {}
+function bookmarkReducer(state = initialState, action) {
+  switch (action.type) {
+    case "ADD_BOOKMARK":
+      return { ...state, bookmarks: [...state.bookmarks, action.payload] };
+    case "REMOVE_BOOKMARK":
+      return {
+        ...state,
+        bookmarks: state.bookmarks.filter(
+          (bookmark) => bookmark.id !== action.payload.id
+        ),
+      };
+    case "UPDATE_TAG":
+      return {
+        ...state,
+        bookmarks: state.bookmarks.map((bookmark) => {
+          if (bookmark.id === action.payload.bookmarkId) {
+            return {
+              ...bookmark,
+              tags: bookmark.tags.map((tag) =>
+                tag.id === action.payload.updatedTag.id
+                  ? { ...action.payload.updatedTag }
+                  : tag
+              ),
+            };
+          }
+          return bookmark;
+        }),
+      };
+    case "ADD_TAG":
+      return {
+        ...state,
+        bookmarks: state.bookmarks.map((bookmark) =>
+          bookmark.id === action.payload.bookmarkId
+            ? { ...bookmark, tags: [...bookmark.tags, action.payload.tag] }
+            : bookmark
+        ),
+      };
+    case "REMOVE_TAG":
+      return {
+        ...state,
+        bookmarks: state.bookmarks.map((bookmark) =>
+          bookmark.id === action.payload.bookmarkId
+            ? {
+                ...bookmark,
+                tags: bookmark.tags.filter(
+                  (tag) => tag.id !== action.payload.tagId
+                ),
+              }
+            : bookmark
+        ),
+      };
+    default:
+      return { ...state };
+      break;
+  }
+}
 
 export default bookmarkReducer;
--- src/ex04/bookmarkReducer/bookmark.reducer.test.js
+++ src/ex04/bookmarkReducer/bookmark.reducer.test.js
@@ -1,1 +1,167 @@
 import bookmarkReducer from "./bookmark.reducer";
+
+const initialState = {
+  bookmarks: [{ id: 1, title: "Neogcamp", Url: "abc.com" }],
+  filters: {},
+};
+
+describe("bookmarkReducer", () => {
+  it("should add bookmark to the array of bookmarks", () => {
+    const initialState = {
+      bookmarks: [{ id: 1, title: "Neogcamp", Url: "abc.com", tags: [] }],
+      filters: {},
+    };
+    const action = {
+      type: "ADD_BOOKMARK",
+      payload: { id: 2, title: "gautam kumar", Url: "gautam.com", tags: [] },
+    };
+
+    const updatedState = bookmarkReducer(initialState, action);
+
+    expect(updatedState).toEqual({
+      bookmarks: [
+        { id: 1, title: "Neogcamp", Url: "abc.com", tags: [] },
+        { id: 2, title: "gautam kumar", Url: "gautam.com", tags: [] },
+      ],
+      filters: {},
+    });
+  });
+  it("should remove bookmark from array of bookmarks", () => {
+    const initialState = {
+      bookmarks: [
+        { id: 1, title: "Neogcamp", Url: "abc.com", tags: [] },
+        { id: 2, title: "gautam kumar", Url: "gautam.com", tags: [] },
+      ],
+      filters: {},
+    };
+    const action = {
+      type: "REMOVE_BOOKMARK",
+      payload: { id: 2, title: "gautam kumar", Url: "gautam.com", tags: [] },
+    };
+
+    const updatedState = bookmarkReducer(initialState, action);
+
+    expect(updatedState).toEqual({
+      bookmarks: [{ id: 1, title: "Neogcamp", Url: "abc.com", tags: [] }],
+      filters: {},
+    });
+  });
+  it("should update tags to the bookmark ", () => {
+    const initialState = {
+      bookmarks: [
+        { id: 1, title: "Neogcamp", Url: "abc.com", tags: [] },
+        {
+          id: 2,
+          title: "gautam kumar",
+          Url: "gautam.com",
+          tags: [{ id: 1, tag: "parsonal" }],
+        },
+      ],
+      filters: {},
+    };
+    const action = {
+      type: "UPDATE_TAG",
+      payload: {
+        bookmarkId: 2,
+        updatedTag: { id: 1, tag: "portfolio" },
+      },
+    };
+
+    const updatedState = bookmarkReducer(initialState, action);
+
+    expect(updatedState).toEqual({
+      bookmarks: [
+        { id: 1, title: "Neogcamp", Url: "abc.com", tags: [] },
+        {
+          id: 2,
+          title: "gautam kumar",
+          Url: "gautam.com",
+          tags: [{ id: 1, tag: "portfolio" }],
+        },
+      ],
+      filters: {},
+    });
+  });
+  it("should add tag to the bookmark ", () => {
+    const initialState = {
+      bookmarks: [
+        { id: 1, title: "Neogcamp", Url: "abc.com", tags: [] },
+        {
+          id: 2,
+          title: "gautam kumar",
+          Url: "gautam.com",
+          tags: [{ id: 1, tag: "portfolio" }],
+        },
+      ],
+      filters: {},
+    };
+    const action = {
+      type: "ADD_TAG",
+      payload: {
+        bookmarkId: 1,
+        tag: { id: 1, tag: "learning" },
+      },
+    };
+
+    const updatedState = bookmarkReducer(initialState, action);
+
+    expect(updatedState).toEqual({
+      bookmarks: [
+        {
+          id: 1,
+          title: "Neogcamp",
+          Url: "abc.com",
+          tags: [{ id: 1, tag: "learning" }],
+        },
+        {
+          id: 2,
+          title: "gautam kumar",
+          Url: "gautam.com",
+          tags: [{ id: 1, tag: "portfolio" }],
+        },
+      ],
+      filters: {},
+    });
+  });
+  it("should remove tag from bookmark tags ", () => {
+    const initialState = {
+      bookmarks: [
+        {
+          id: 1,
+          title: "Neogcamp",
+          Url: "abc.com",
+          tags: [{ id: 1, tag: "learning" }],
+        },
+        {
+          id: 2,
+          title: "gautam kumar",
+          Url: "gautam.com",
+          tags: [{ id: 1, tag: "portfolio" }],
+        },
+      ],
+      filters: {},
+    };
+    const action = {
+      type: "REMOVE_TAG",
+      payload: {
+        bookmarkId: 1,
+        tagId: 1,
+      },
+    };
+
+    const updatedState = bookmarkReducer(initialState, action);
+
+    expect(updatedState).toEqual({
+      bookmarks: [
+        { id: 1, title: "Neogcamp", Url: "abc.com", tags: [] },
+        {
+          id: 2,
+          title: "gautam kumar",
+          Url: "gautam.com",
+          tags: [{ id: 1, tag: "portfolio" }],
+        },
+      ],
+      filters: {},
+    });
+  });
+});
