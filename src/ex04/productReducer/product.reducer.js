436e7e249476f7df4599fdf7cb2fa86d33528dad
gkumar-pandey <gautamshekhar078@gmail.com>
2023-11-16 20:21:12 +0530

    product reducer test done

--- src/ex04/productReducer/product.reducer.js
+++ src/ex04/productReducer/product.reducer.js
@@ -1,5 +1,78 @@
-const initialState = {};
+const initialState = {
+  products: [
+    {
+      id: 1,
+      name: "Phone",
+      category: "Electronics",
+      price: 500,
+      inStock: true,
+    },
+    { id: 2, name: "Shirt", category: "Clothing", price: 20, inStock: true },
+    {
+      id: 3,
+      name: "Laptop",
+      category: "Electronics",
+      price: 1000,
+      inStock: true,
+    },
+    { id: 4, name: "Jeans", category: "Clothing", price: 40, inStock: false },
+  ],
+  filters: {
+    category: "All",
+    searchQuery: "",
+    sortBy: "price",
+    ascending: true,
+    priceRange: { min: 0, max: 1000 },
+  },
+};
 
-function productReducer(state = initialState, action) {}
+function productReducer(state = initialState, action) {
+  switch (action.type) {
+    case "SET_CATEGORY":
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          category: action.payload,
+        },
+      };
+    case "SET_SEARCH_QUERY":
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          searchQuery: action.payload,
+        },
+      };
+    case "SET_SORT":
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          sortBy: action.payload,
+        },
+      };
+    case "SET_PRICE_RANGE":
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          priceRange: action.payload,
+        },
+      };
+    case "TOGGLE_AVAILABILITY":
+      return {
+        ...state,
+        products: state.products.map((product) => {
+          return product.id === action.payload.productId
+            ? { ...product, inStock: !product.inStock }
+            : product;
+        }),
+      };
+
+    default:
+      break;
+  }
+}
 
 export default productReducer;
--- src/ex04/productReducer/product.reducer.test.js
+++ src/ex04/productReducer/product.reducer.test.js
@@ -1,1 +1,301 @@
 import productReducer from "./product.reducer";
+const initialState = {
+  products: [
+    {
+      id: 1,
+      name: "Phone",
+      category: "Electronics",
+      price: 500,
+      inStock: true,
+    },
+    { id: 2, name: "Shirt", category: "Clothing", price: 20, inStock: true },
+    {
+      id: 3,
+      name: "Laptop",
+      category: "Electronics",
+      price: 1000,
+      inStock: true,
+    },
+    { id: 4, name: "Jeans", category: "Clothing", price: 40, inStock: false },
+  ],
+  filters: {
+    category: "All",
+    searchQuery: "",
+    sortBy: "price",
+    ascending: true,
+    priceRange: { min: 0, max: 1000 },
+  },
+};
+
+describe("Product Reducer", () => {
+  it("should set category filter ", () => {
+    const initialState = {
+      products: [
+        {
+          id: 1,
+          name: "Phone",
+          category: "Electronics",
+          price: 500,
+          inStock: true,
+        },
+        {
+          id: 2,
+          name: "Shirt",
+          category: "Clothing",
+          price: 20,
+          inStock: true,
+        },
+        {
+          id: 3,
+          name: "Laptop",
+          category: "Electronics",
+          price: 1000,
+          inStock: true,
+        },
+        {
+          id: 4,
+          name: "Jeans",
+          category: "Clothing",
+          price: 40,
+          inStock: false,
+        },
+      ],
+      filters: {
+        category: "All",
+        searchQuery: "",
+        sortBy: "price",
+        ascending: true,
+        priceRange: { min: 0, max: 1000 },
+      },
+    };
+
+    const action = {
+      type: "SET_CATEGORY",
+      payload: "Cloathing",
+    };
+    const updatedState = productReducer(initialState, action);
+    expect(updatedState).toEqual({
+      products: [
+        {
+          id: 1,
+          name: "Phone",
+          category: "Electronics",
+          price: 500,
+          inStock: true,
+        },
+        {
+          id: 2,
+          name: "Shirt",
+          category: "Clothing",
+          price: 20,
+          inStock: true,
+        },
+        {
+          id: 3,
+          name: "Laptop",
+          category: "Electronics",
+          price: 1000,
+          inStock: true,
+        },
+        {
+          id: 4,
+          name: "Jeans",
+          category: "Clothing",
+          price: 40,
+          inStock: false,
+        },
+      ],
+      filters: {
+        category: "Cloathing",
+        searchQuery: "",
+        sortBy: "price",
+        ascending: true,
+        priceRange: { min: 0, max: 1000 },
+      },
+    });
+  });
+  it("should set search query ", () => {
+    const action = {
+      type: "SET_SEARCH_QUERY",
+      payload: "phone",
+    };
+    const updatedState = productReducer(initialState, action);
+    expect(updatedState).toEqual({
+      products: [
+        {
+          id: 1,
+          name: "Phone",
+          category: "Electronics",
+          price: 500,
+          inStock: true,
+        },
+        {
+          id: 2,
+          name: "Shirt",
+          category: "Clothing",
+          price: 20,
+          inStock: true,
+        },
+        {
+          id: 3,
+          name: "Laptop",
+          category: "Electronics",
+          price: 1000,
+          inStock: true,
+        },
+        {
+          id: 4,
+          name: "Jeans",
+          category: "Clothing",
+          price: 40,
+          inStock: false,
+        },
+      ],
+      filters: {
+        category: "All",
+        searchQuery: "phone",
+        sortBy: "price",
+        ascending: true,
+        priceRange: { min: 0, max: 1000 },
+      },
+    });
+  });
+  it("should set sort", () => {
+    const action = {
+      type: "SET_SORT",
+      payload: "inStock",
+    };
+    const updatedState = productReducer(initialState, action);
+    expect(updatedState).toEqual({
+      products: [
+        {
+          id: 1,
+          name: "Phone",
+          category: "Electronics",
+          price: 500,
+          inStock: true,
+        },
+        {
+          id: 2,
+          name: "Shirt",
+          category: "Clothing",
+          price: 20,
+          inStock: true,
+        },
+        {
+          id: 3,
+          name: "Laptop",
+          category: "Electronics",
+          price: 1000,
+          inStock: true,
+        },
+        {
+          id: 4,
+          name: "Jeans",
+          category: "Clothing",
+          price: 40,
+          inStock: false,
+        },
+      ],
+      filters: {
+        category: "All",
+        searchQuery: "",
+        sortBy: "inStock",
+        ascending: true,
+        priceRange: { min: 0, max: 1000 },
+      },
+    });
+  });
+  it("should set price range ", () => {
+    const action = {
+      type: "SET_PRICE_RANGE",
+      payload: { min: 20, max: 500 },
+    };
+    const updatedState = productReducer(initialState, action);
+    expect(updatedState).toEqual({
+      products: [
+        {
+          id: 1,
+          name: "Phone",
+          category: "Electronics",
+          price: 500,
+          inStock: true,
+        },
+        {
+          id: 2,
+          name: "Shirt",
+          category: "Clothing",
+          price: 20,
+          inStock: true,
+        },
+        {
+          id: 3,
+          name: "Laptop",
+          category: "Electronics",
+          price: 1000,
+          inStock: true,
+        },
+        {
+          id: 4,
+          name: "Jeans",
+          category: "Clothing",
+          price: 40,
+          inStock: false,
+        },
+      ],
+      filters: {
+        category: "All",
+        searchQuery: "",
+        sortBy: "price",
+        ascending: true,
+        priceRange: { min: 20, max: 500 },
+      },
+    });
+  });
+  it("should toggle availability of the product", () => {
+    const action = {
+      type: "TOGGLE_AVAILABILITY",
+      payload: { productId: 2 },
+    };
+    const updatedState = productReducer(initialState, action);
+    expect(updatedState).toEqual({
+      products: [
+        {
+          id: 1,
+          name: "Phone",
+          category: "Electronics",
+          price: 500,
+          inStock: true,
+        },
+        {
+          id: 2,
+          name: "Shirt",
+          category: "Clothing",
+          price: 20,
+          inStock: false,
+        },
+        {
+          id: 3,
+          name: "Laptop",
+          category: "Electronics",
+          price: 1000,
+          inStock: true,
+        },
+        {
+          id: 4,
+          name: "Jeans",
+          category: "Clothing",
+          price: 40,
+          inStock: false,
+        },
+      ],
+      filters: {
+        category: "All",
+        searchQuery: "",
+        sortBy: "price",
+        ascending: true,
+        priceRange: { min: 0, max: 1000 },
+      },
+    });
+  });
+});
